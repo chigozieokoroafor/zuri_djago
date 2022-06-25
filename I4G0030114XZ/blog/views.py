6ed3815d7414639a851d2626090160525b04a362
chigozieokoroafor <okoroaforc14@gmail.com>
2022-06-26 00:11:18 +0100

    done

--- I4G0030114XZ/blog/views.py
+++ I4G0030114XZ/blog/views.py
@@ -7,21 +7,26 @@ from .models import Post
 
 class PostListView(ListView):
     model = Post
+    template_name = "post_list.html"
 
 class PostCreateView(CreateView):
     model = Post
     fields = "__all__"
+    template_name = "post_form.html"
     success_url = reverse_lazy("blog:all")
 
 class PostDetailView(DetailView):
     model = Post
+    template_name = "post_detail.html"
 
 class PostUpdateView(UpdateView):
     model = Post
     fields = "__all__"
+    template_name = "post_form.html"
     success_url: reverse_lazy("blog:all")
 
 class PostDeleteView(UpdateView):
     model = Post
     fields = "__all__"
+    template_name = "post_confirm_delete.html"
     success_url = reverse_lazy("blog:all")
